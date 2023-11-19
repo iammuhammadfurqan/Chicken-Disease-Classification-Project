9edf75665336f12ed6b49b5e75062cf64b09225a
iammuhammadfurqan <sheikhfurqan048@gmail.com>
2023-11-20 02:47:41 +0500

    Evaluation and App Completed

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,9 @@
 import os
 from cnnClassifier.constants import *
+from pathlib import Path
 from cnnClassifier.utils.common import read_yaml , create_directories
-from cnnClassifier.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig , PrepareCallbacksConfig ,TrainingConfig)
+from cnnClassifier.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig , PrepareCallbacksConfig 
+                                                ,TrainingConfig, EvaluationConfig)
 
 class ConfigurationManager:
     def __init__(self,
@@ -84,3 +86,13 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model=Path("artifacts/training/model.h5"),
+            training_data=Path("artifacts/data_ingestion/Chicken-fecal-images"),
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
